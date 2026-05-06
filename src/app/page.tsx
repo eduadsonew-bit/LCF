e3a4c508f3079ed995e227a499a7d796b39f1bfd
Z User <z@container>
2026-05-06 00:44:38 +0000

    7ab56a4a-7222-4d6e-b706-c3e9516410bc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -223,7 +223,7 @@ function SponsorsCarousel({ sponsors }: { sponsors: Sponsor[] }) {
                       src={sponsor.logo}
                       alt={sponsor.name}
                       className="relative z-10 mix-blend-multiply"
-                      style={{ width: '90%', height: '90%', objectFit: 'contain', objectPosition: '70% 0%' }}
+                      style={{ width: '90%', height: '90%', objectFit: 'contain', objectPosition: '30% 0%' }}
                     />
                   </div>
                 ) : (
